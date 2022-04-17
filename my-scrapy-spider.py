69d7765fe0bea84d654ab389be32fb7192175d11
loveencounterflow <loveencounterflow@example.com>
2022-04-17 13:45:12 +0200

    update scrapy spider

--- my-scrapy-spider.py
+++ my-scrapy-spider.py
@@ -1,14 +1,19 @@
 import scrapy
 
 class BlogSpider(scrapy.Spider):
-  name = 'blogspider'
-  start_urls = ['https://www.zyte.com/blog/']
+  name        = 'blogspider'
+  url         = 'https://www.zvg-online.net/1400/1101986118/ag_seite_001.php'
+  start_urls  = [ url, ]
 
-  def parse(self, response):
-    for title in response.css('.oxy-post-title'):
-      yield {'title': title.css('::text').get()}
+  def parse( self, response ):
+    print( "######################### scraping #############################" )
+    nr = 0
+    for container in response.css('.container_vors'):
+      nr += 1
+      for link in response.css( 'a' ):
+        yield { 'nr': nr, 'text': container.css( '::text' ).get(), }
 
-    for next_page in response.css('a.next'):
-      yield response.follow(next_page, self.parse)
+    # for next_page in response.css('a.next'):
+    #   yield response.follow(next_page, self.parse)
 
 
